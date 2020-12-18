4fc66d4d70eaa282a026777058db49e18c3e572c
unknown <sharifislam5@gmail.com>
2020-12-18 20:48:54 +0600

    changes

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'module';
+import './App.css';
+
+function App() {
+    return <div className="App">Hello</div>;
+}
+
+export default App;
---
+++ src/components/header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Header = () => (
+    <div>
+        <header>
+            <div className="container react" id="header">
+                <div>
+                    <img className="logo" src="images/logo.svg" alt="Logo" />
+                </div>
+                <div className="textRight">
+                    <img src="images/react.svg" alt="React" className="reactLogo" />
+                    <strong>React</strong>
+                </div>
+            </div>
+        </header>
+    </div>
+);
+
+export default Header;
